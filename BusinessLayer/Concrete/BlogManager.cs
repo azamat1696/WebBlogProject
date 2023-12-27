2d6e489feb43a92ecb8e716a5af301fec5f4c2ce
Azamat1696 <azamat1696@gmail.com>
2023-12-27 11:12:12 +0200

    #Ders 27

---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class BlogManager : IBlogService
+	{
+		IBlogDal _blogDal;
+
+		public BlogManager(IBlogDal blogDal)
+		{
+			_blogDal = blogDal;
+		}
+
+		public void BlogAdd(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void BlogRemove(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void BlogUpdate(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Blog> GetAllBlogs()
+		{
+			return _blogDal.GetAll();
+		}
+
+		public Blog GetBlogById(int id)
+		{
+			return _blogDal.GetByID(id);
+		}
+
+		public List<Blog> GetBlogsWithCategory()
+		{
+			return _blogDal.GetBlogsWithCategories();
+		}
+	}
+}
---
+++ WebBlogProject/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebBlogProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+            var values = _blogManager.GetBlogsWithCategory();
+            return View(values);
+        }
+        public IActionResult BlogDetails(int id)
+        {
+            var value = _blogManager.GetBlogById(id);
+            return View(value);
+        }
+    }
+}
